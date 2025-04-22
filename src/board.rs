e60d102f968471b50ae766d7d30fbb0652e0ab23
tolumide-ng <tolumideshopein@gmail.com>
2025-04-23 01:31:33 +0200

    Adds some methods for piece identification on the bitboard

--- src/board.rs
+++ src/board.rs
@@ -1,18 +1,36 @@
 #[derive(Debug)]
 pub(crate) enum Color {
-    White, Black
+    White,
+    Black,
 }
 
 pub(crate) struct Board {
+    /// white pieces and white kings
     white: u32,
+    /// black pieces pieces and black kings
     black: u32,
+    /// black and white kings
     kings: u32,
+    turn: Color,
 }
 
-
 impl Board {
-    pub(crate) fn regular(&self, color: Color) {
-        
+    /// returns the positions of the kings of the provided color on the board
+    pub(crate) fn kings(&self, color: Color) -> u32 {
+        match color {
+            Color::Black => self.black & self.kings,
+            Color::White => self.white & self.kings,
+        }
     }
-    pub(crate) fn kings(&self, color: Color) {}
+
+    /// Returns the positions of the regular members for a specific color, excluding the kings on the board
+    pub(crate) fn regular(&self, color: Color) -> u32 {
+        match color {
+            Color::Black => self.black & !self.kings,
+            Color::White => self.white & !self.kings,
+        }
+    }
+
+    /// returns all the possible options a selected piece can play
+    pub(crate) fn options(&self, color: Color, piece: u32) {}
 }
