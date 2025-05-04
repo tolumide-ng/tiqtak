9a0dea27b153fa3d393f4ea20cc375ee7e172914
tolumide-ng <tolumideshopein@gmail.com>
2025-05-04 18:34:24 +0200

    Minor docs update

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+//! TiqTak is a fast, modern, and portable checkers engine, built with Rust and exposed to the web
+//! via WebAssembly.
+//! At its core, this engine leverage Monte Carlo Tree Search (MCTS) to provide adapative, probabilistic
+//! decision-making that balance exploration and exploitation-making it well suited for both casual play,
+//! and competitive AI development.
+//! In the future, I plan to make the search heuristic used more dynamic such that it canbe provide  
+//! by the user, rather than been forced to use the choices made by this author (me).
+
 pub mod game;
 pub(crate) mod mcts;
